a6a4093a3372af7b07019754b4ea328a41e9638c
Wojciech Domżał <wojciech.domzal03@gmail.com>
2022-07-25 21:37:09 +0200

    Add error page, ordering data coming from form step 2.

--- routers/vlsm-calc.ts
+++ routers/vlsm-calc.ts
@@ -1,5 +1,6 @@
 import {Router} from "express";
 import {CookieObject} from "../data-entity/cookie-object";
+import {Subnets} from "../types/data-entity";
 
 export const vlsm = Router();
 
@@ -9,13 +10,19 @@ vlsm
         res.render('form/send-form');
     })
     .post('/step1', ((req, res) => {
-        const newCookie = new CookieObject({
-            url: req.originalUrl,
-            ...req.body,
-        });
-        res
-            .cookie('currentCalculation', JSON.stringify(newCookie))
-            .redirect('/step2');
+        try {
+            const newCookie = new CookieObject({
+                url: req.originalUrl,
+                ...req.body,
+            });
+            res
+                .cookie('currentCalculation', JSON.stringify(newCookie))
+                .redirect('/step2');
+        } catch (error) {
+            res.render('error', {
+                description: error,
+            })
+        }
     }))
     .get('/step2', ((req, res) => {
         const {currentCalculation} = req.cookies as {
@@ -32,7 +39,31 @@ vlsm
             res.clearCookie('currentCalculation')
                 .redirect('/step1');
         }
+    }))
+    .post('/summary', ((req, res) => {
+        const receivedBodyArray = Object.keys(req.body)
+            .map((key) => {
+                return req.body[key];
+            });
+        const subnets: Array<Subnets> = []
+        const orderingData = (spliceStart: number = 0, spliceCount: number = 2): Array<string> | null => {
+            const copyArray = [...receivedBodyArray];
+            const [hostName, hostAmount] = ((copyArray.splice(spliceStart, spliceCount) as Array<string>));
 
+            subnets.push({
+                hostName: hostName,
+                hostAmount: Number(hostAmount),
+            })
+
+            if (spliceStart < (receivedBodyArray.length - 2)) {
+                orderingData(spliceStart + 2)
+            } else {
+                return null;
+            }
+        }
+        orderingData();
+
+        res.send('ok');
     }))
     .get('*', ((req, res) => {
         res.redirect('/step1');
--- utils/error.ts
+++ utils/error.ts
@@ -5,10 +5,10 @@ export class ValidationError extends Error {
 
 export const handleError = (err: Error, req: Request, res: Response, next: NextFunction) => {
     console.error(err);
-
+    const message = err instanceof ValidationError ? err.message : 'Sorry, please try again later.'
     res
         .status(err instanceof ValidationError ? 400 : 500)
         .json({
-            message: err instanceof ValidationError ? err.message : 'Sorry, please try again later.',
+            message,
         });
 }
